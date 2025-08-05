87e886da2d3e17fca166d6e67cf27e5691e2f4ce
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-08-05 20:42:21 +0700

    Update fitur

--- app/Http/Controllers/BarangAPIController.php
+++ app/Http/Controllers/BarangAPIController.php
@@ -8,28 +8,29 @@ use Illuminate\Http\Request;
 class BarangAPIController extends Controller
 {
     public function index() {
-        $barang = Barang::all();
-
-        return response()->json([
-            'status' => 'success',
-            'data' => $barang
-        ]);
+        return response()->json(Barang::all(), 200);
     }
 
     public function show($kode) {
         $barang = Barang::where('kode_barang', $kode)->first();
-
         if (!$barang) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Barang tidak ditemukan'
-            ], 404);
+            return response()->json(['message' => 'Barang tidak ditemukan'], 404);
         }
-
-        return response()->json([
-            'status' => 'success',
-            'data' => $barang
-        ]);
+        return response()->json($barang, 200);
     }
 
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'kode_barang' => 'required|unique:barangs,kode_barang',
+            'nama_barang' => 'required',
+            'harga' => 'required|integer|min:0',
+        ]);
+
+        $barang = Barang::create($validated);
+
+        return response()->json([
+            'message' => 'Barang berhasil ditambahkan',
+            'data' => $barang
+        ], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('barang')->group(function () {
     Route::get('/', [BarangAPIController::class, 'index']);
     Route::get('/{kode}', [BarangAPIController::class, 'show']);
+    Route::post('/', [BarangAPIController::class, 'store']);
 });
 
